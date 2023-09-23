c2b56c1bfcf1616ed32d41973b4aefa7529dffbe
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-09-22 23:56:01 -0400

    Project is basically finished

--- src/main/java/lemon_juice/better_berries_plus/item/custom/JamItem.java
+++ src/main/java/lemon_juice/better_berries_plus/item/custom/JamItem.java
@@ -1,7 +1,20 @@
 package lemon_juice.better_berries_plus.item.custom;
 
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
 public class JamItem extends DrinkItem{
     public JamItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public boolean hasCraftingRemainingItem(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getCraftingRemainingItem(ItemStack itemStack) {
+        return new ItemStack(Items.GLASS_BOTTLE);
     }
 }
